1050249cdfd238130d84b18f325332c7fd2ef3e6
Nguyen Luong Khang <96479575+Khangnekk@users.noreply.github.com>
2024-09-27 17:30:14 +0700

    release v2.1.3 (full function)

--- BatchVideoEditing/BatchVideoEditing/Form1.Designer.cs
+++ BatchVideoEditing/BatchVideoEditing/Form1.Designer.cs
@@ -8,8 +8,8 @@
 		private System.Windows.Forms.Button btnGoToPath;
 		private System.Windows.Forms.Button btnClearAll;
 		private System.Windows.Forms.ListBox lstVideos;
-		private System.Windows.Forms.ProgressBar progressBar;
-		private System.Windows.Forms.Label lblProgress;
+		private System.Windows.Forms.ProgressBar totalprogressBar;
+		private System.Windows.Forms.Label totallblProgress;
 		private System.Windows.Forms.FlowLayoutPanel flowLayoutPanelProgress;  
 		// Dispose method
 		protected override void Dispose(bool disposing)
@@ -29,11 +29,17 @@
 			btnGoToPath = new Button();
 			btnClearAll = new Button();
 			lstVideos = new ListBox();
-			progressBar = new ProgressBar();
-			lblProgress = new Label();
+			totalprogressBar = new ProgressBar();
+			totallblProgress = new Label();
 			label1 = new Label();
 			flowLayoutPanelProgress = new FlowLayoutPanel();
 			label2 = new Label();
+			label3 = new Label();
+			comboBox1 = new ComboBox();
+			label4 = new Label();
+			label5 = new Label();
+			label6 = new Label();
+			label7 = new Label();
 			SuspendLayout();
 			// 
 			// btnImport
@@ -68,17 +74,16 @@
 			btnGoToPath.TabIndex = 7;
 			btnGoToPath.Text = "Xem kết quả";
 			btnGoToPath.UseVisualStyleBackColor = true;
-			btnGoToPath.Visible = false;
 			btnGoToPath.Click += btnGoToPath_Click;
 			// 
 			// btnClearAll
 			// 
 			btnClearAll.Cursor = Cursors.Hand;
-			btnClearAll.Location = new Point(35, 363);
+			btnClearAll.Location = new Point(523, 106);
 			btnClearAll.Name = "btnClearAll";
 			btnClearAll.Size = new Size(150, 31);
 			btnClearAll.TabIndex = 9;
-			btnClearAll.Text = "  Clear All";
+			btnClearAll.Text = " Reset all";
 			btnClearAll.TextAlign = ContentAlignment.MiddleLeft;
 			btnClearAll.UseVisualStyleBackColor = true;
 			btnClearAll.Click += btnClearAll_Click;
@@ -92,21 +97,21 @@
 			lstVideos.Size = new Size(638, 184);
 			lstVideos.TabIndex = 3;
 			// 
-			// progressBar
+			// totalprogressBar
 			// 
-			progressBar.Location = new Point(703, 585);
-			progressBar.Name = "progressBar";
-			progressBar.Size = new Size(586, 23);
-			progressBar.TabIndex = 2;
+			totalprogressBar.Location = new Point(703, 585);
+			totalprogressBar.Name = "totalprogressBar";
+			totalprogressBar.Size = new Size(586, 23);
+			totalprogressBar.TabIndex = 2;
 			// 
-			// lblProgress
+			// totallblProgress
 			// 
-			lblProgress.AutoSize = true;
-			lblProgress.Location = new Point(703, 561);
-			lblProgress.Name = "lblProgress";
-			lblProgress.Size = new Size(129, 20);
-			lblProgress.TabIndex = 1;
-			lblProgress.Text = "Total Progress: 0%";
+			totallblProgress.AutoSize = true;
+			totallblProgress.Location = new Point(703, 561);
+			totallblProgress.Name = "totallblProgress";
+			totallblProgress.Size = new Size(129, 20);
+			totallblProgress.TabIndex = 1;
+			totallblProgress.Text = "Total Progress: 0%";
 			// 
 			// label1
 			// 
@@ -137,15 +142,81 @@
 			label2.TabIndex = 10;
 			label2.Text = "(*) Những video được xử lý sẽ xuất hiện ở đây";
 			// 
+			// label3
+			// 
+			label3.AutoSize = true;
+			label3.ForeColor = SystemColors.AppWorkspace;
+			label3.Location = new Point(12, 644);
+			label3.Name = "label3";
+			label3.Size = new Size(94, 20);
+			label3.TabIndex = 11;
+			label3.Text = "Version: 2.1.3";
+			// 
+			// comboBox1
+			// 
+			comboBox1.FormattingEnabled = true;
+			comboBox1.Items.AddRange(new object[] { " Xử lý đồng thời", " Xử lý tuần tự" });
+			comboBox1.Location = new Point(1138, 115);
+			comboBox1.Name = "comboBox1";
+			comboBox1.Size = new Size(151, 28);
+			comboBox1.TabIndex = 12;
+			comboBox1.SelectedIndexChanged += comboBox1_SelectedIndexChanged;
+			// 
+			// label4
+			// 
+			label4.AutoSize = true;
+			label4.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+			label4.Location = new Point(35, 482);
+			label4.Name = "label4";
+			label4.Size = new Size(551, 19);
+			label4.TabIndex = 13;
+			label4.Text = "Xử lý tuần tự sẽ thích hợp với máy yếu, chậm nhưng không giới hạn số video import vào";
+			// 
+			// label5
+			// 
+			label5.AutoSize = true;
+			label5.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+			label5.Location = new Point(35, 514);
+			label5.Name = "label5";
+			label5.Size = new Size(559, 19);
+			label5.TabIndex = 14;
+			label5.Text = "Xử lý đồng thời tốn nhiều tài nguyên tại 1 thời điểm hơn, đổi lại sẽ tiết kiệm thời gian hơn";
+			// 
+			// label6
+			// 
+			label6.AutoSize = true;
+			label6.Font = new Font("Segoe UI", 8F, FontStyle.Regular, GraphicsUnit.Point);
+			label6.Location = new Point(35, 546);
+			label6.Name = "label6";
+			label6.Size = new Size(306, 19);
+			label6.TabIndex = 15;
+			label6.Text = "Xử lý đồng thời có thể xử lý tối đa 20 video 1 lúc";
+			// 
+			// label7
+			// 
+			label7.AutoSize = true;
+			label7.Font = new Font("Segoe UI", 8F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
+			label7.Location = new Point(35, 453);
+			label7.Name = "label7";
+			label7.Size = new Size(88, 19);
+			label7.TabIndex = 16;
+			label7.Text = "Tips && Trick";
+			// 
 			// Form1
 			// 
 			AutoScaleDimensions = new SizeF(8F, 20F);
 			AutoScaleMode = AutoScaleMode.Font;
 			ClientSize = new Size(1331, 673);
+			Controls.Add(label7);
+			Controls.Add(label6);
+			Controls.Add(label5);
+			Controls.Add(label4);
+			Controls.Add(comboBox1);
+			Controls.Add(label3);
 			Controls.Add(label2);
 			Controls.Add(label1);
-			Controls.Add(lblProgress);
-			Controls.Add(progressBar);
+			Controls.Add(totallblProgress);
+			Controls.Add(totalprogressBar);
 			Controls.Add(lstVideos);
 			Controls.Add(btnProcess);
 			Controls.Add(btnImport);
@@ -156,11 +227,18 @@
 			Name = "Form1";
 			StartPosition = FormStartPosition.CenterScreen;
 			Text = "Batch video editing";
+			Load += Form1_Load;
 			ResumeLayout(false);
 			PerformLayout();
 		}
 
 		private Label label1;
 		private Label label2;
+		private Label label3;
+		private ComboBox comboBox1;
+		private Label label4;
+		private Label label5;
+		private Label label6;
+		private Label label7;
 	}
 }
--- BatchVideoEditing/BatchVideoEditing/Form1.cs
+++ BatchVideoEditing/BatchVideoEditing/Form1.cs
@@ -8,6 +8,9 @@ namespace BatchVideoEditing
 		private List<string> videoPaths;
 		private string audioFolderPath = @"C:\path\to\audio"; // Thay bằng đường dẫn tới folder chứa file âm thanh
 		private string outputFolderPath = "output";
+		private int type = 0;
+		private int totalVideoImportAsync = 0;
+		private int completeVideoAsync = 0;
 
 		public Form1()
 		{
@@ -31,8 +34,8 @@ namespace BatchVideoEditing
 
 		private async void btnProcess_Click(object sender, EventArgs e)
 		{
-			progressBar.Value = 0;
-			lblProgress.Text = "Total Progress: 0%";
+			totalprogressBar.Value = 0;
+			totallblProgress.Text = "Total Progress: 0%";
 
 			if (videoPaths.Count == 0)
 			{
@@ -40,28 +43,95 @@ namespace BatchVideoEditing
 				return;
 			}
 			btnProcess.Enabled = false;
-			await ProcessVideosAsync(videoPaths);
+			comboBox1.Enabled = false;
+			btnImport.Enabled = false;
+			switch (type)
+			{
+				case 0:
+					// xử lý đồng thời
+					await ProcessVideosAsync(videoPaths);
+					break;
+				case 1:
+					// xử lý tuần tự
+					await ProcessVideosAsyncType1(videoPaths);
+					break;
+				default:
+					break;
+			}
 
-			progressBar.Value = 100;
-			lblProgress.Text = "Total Progress: 100%";
+			totalprogressBar.Value = 100;
+			totallblProgress.Text = "Total Progress: 100%";
 			btnGoToPath.Visible = true;
 			btnProcess.Enabled = true;
+			comboBox1.Enabled = true;
+			btnImport.Enabled = true;
 			lstVideos.Items.Clear();
-			//// Xóa hết mọi thứ trong thư mục temp
-			//string tempFolderPath = Path.Combine(outputFolderPath, "temp");
-			//if (Directory.Exists(tempFolderPath))
-			//{
-			//	Directory.Delete(tempFolderPath, true);
-			//}
 		}
 
 		private async Task ProcessVideosAsync(List<string> videos)
+		{
+			int totalVideos = videos.Count;
+			totalVideoImportAsync = totalVideos;
+			int completedVideos = 0;
+			int maxDegreeOfParallelism = 20; // Giới hạn số lượng video xử lý cùng lúc, bạn có thể điều chỉnh số này
+			using (SemaphoreSlim semaphore = new SemaphoreSlim(maxDegreeOfParallelism))
+			{
+				var tasks = new List<Task>();
+
+				foreach (var video in videos)
+				{
+					await semaphore.WaitAsync(); // Đợi cho đến khi có tài nguyên trống
+
+					// Xử lý video trong Task.Run để không làm nghẽn UI thread
+					var videoProcessingTask = Task.Run(async () =>
+					{
+						try
+						{
+							int i = videos.IndexOf(video) + 1;
+							ProgressBar progressBarVideo = new ProgressBar { Maximum = 100, Width = 500 };
+							Label labelVideo = new Label { Text = $"{Path.GetFileName(video)}: 0%", Width = 500 };
+
+							flowLayoutPanelProgress.Invoke(new Action(() =>
+							{
+								flowLayoutPanelProgress.Controls.Add(labelVideo);
+								flowLayoutPanelProgress.Controls.Add(progressBarVideo);
+							}));
+
+							await ProcessVideoAsync(video, progressBarVideo, labelVideo, i);
+
+							// Cập nhật tiến độ tổng sau khi hoàn thành từng video
+							Interlocked.Increment(ref completedVideos); // Tăng completedVideos một cách an toàn trong môi trường đa luồng
+							int totalProgressPercentage = (completedVideos * 100) / totalVideos;
+
+							// Cập nhật tổng thanh tiến độ và nhãn
+							totalprogressBar.Invoke(new Action(() =>
+							{
+								totalprogressBar.Value = totalProgressPercentage;
+								totallblProgress.Text = $"Total Progress: {totalProgressPercentage}%";
+							}));
+						}
+						finally
+						{
+							semaphore.Release(); // Giải phóng tài nguyên để task khác có thể chạy
+						}
+					});
+
+					tasks.Add(videoProcessingTask);
+				}
+
+				// Chờ tất cả các video xử lý xong
+				await Task.WhenAll(tasks);
+			}
+		}
+
+		private async Task ProcessVideosAsyncType1(List<string> videos)
 		{
 			int totalVideos = videos.Count;
 			int completedVideos = 0;
 
 			foreach (var video in videos)
 			{
+				int i = videos.IndexOf(video) + 1;
 				ProgressBar progressBarVideo = new ProgressBar { Maximum = 100, Width = 500 };
 				Label labelVideo = new Label { Text = $"{Path.GetFileName(video)}: 0%", Width = 500 };
 
@@ -72,27 +142,27 @@ namespace BatchVideoEditing
 				}));
 
 				// Process each video sequentially
-				await ProcessVideoAsync(video, progressBarVideo, labelVideo);
+				await ProcessVideoAsyncType1(video, progressBarVideo, labelVideo, i);
 
 				// Cập nhật tổng tiến độ sau khi hoàn thành từng video
 				completedVideos++;
 				int totalProgressPercentage = (completedVideos * 100) / totalVideos;
 
 				// Cập nhật tổng thanh tiến độ và nhãn
-				progressBar.Value = totalProgressPercentage;
-				lblProgress.Text = $"Total Progress: {totalProgressPercentage}%";
+				totalprogressBar.Value = totalProgressPercentage;
+				totallblProgress.Text = $"Total Progress: {totalProgressPercentage}%";
 			}
 		}
 
 
-		private async Task ProcessVideoAsync(string inputPath, ProgressBar progressBar, Label label)
+		private async Task ProcessVideoAsyncType1(string inputPath, ProgressBar progressBar, Label label, int indexVideo)
 		{
 			try
 			{
 				string outputPath = Path.Combine(outputFolderPath, Path.GetFileNameWithoutExtension(inputPath) + "_processed.mp4");
 
 				// Perform video processing synchronously
-				await Task.Run(() => ProcessVideo(inputPath, outputPath, progressBar, label));
+				await Task.Run(() => ProcessVideo(inputPath, outputPath, progressBar, label, indexVideo));
 			}
 			catch (Exception ex)
 			{
@@ -103,6 +173,27 @@ namespace BatchVideoEditing
 				}));
 			}
 		}
+
+		private async Task ProcessVideoAsync(string inputPath, ProgressBar progressBar, Label label, int i)
+		{
+			try
+			{
+				string outputPath = Path.Combine(outputFolderPath, Path.GetFileNameWithoutExtension(inputPath) + i + "_processed.mp4");
+
+				// Xử lý video không đồng bộ
+				await Task.Run(() => ProcessVideo(inputPath, outputPath, progressBar, label, i));
+			}
+			catch (Exception ex)
+			{
+				this.Invoke(new Action(() =>
+				{
+					// Hiển thị thông báo lỗi nếu có lỗi khi xử lý video
+					MessageBox.Show($"Error processing {Path.GetFileName(inputPath)}: {ex.Message}");
+				}));
+			}
+		}
+
+
 
 
 		private void UpdateVideoProgress(ProgressBar progressBar, Label label, int progressPercentage)
@@ -119,7 +210,7 @@ namespace BatchVideoEditing
 			}
 		}
 
-		private void ProcessVideo(string inputPath, string outputPath, ProgressBar progressBar, Label label)
+		private void ProcessVideo(string inputPath, string outputPath, ProgressBar progressBar, Label label, int videoIndex)
 		{
 			UpdateVideoProgress(progressBar, label, 10);
 			// Đảm bảo thư mục đầu ra tồn tại
@@ -133,8 +224,8 @@ namespace BatchVideoEditing
 			Directory.CreateDirectory(finalFolderPath);
 
 			// 1. Crop video lại sao cho chiều dài giảm xuống còn 80% và thêm nền với hiệu ứng blur
-			string cropAndBlurPath = Path.Combine(tempFolderPath, "cropped_blurred_" + Path.GetFileName(inputPath));
-			CropAndBlurVideo(inputPath, cropAndBlurPath);
+			string cropAndBlurPath = Path.Combine(tempFolderPath, "cropped_blurred_" + videoIndex + "_" + Path.GetFileName(inputPath));
+			CropAndBlurVideo(inputPath, cropAndBlurPath, videoIndex);
 			UpdateVideoProgress(progressBar, label, 20); // Cập nhật tiến độ sau khi hoàn thành bước 1
 
 			// 2. Cắt video đã chỉnh sửa thành 5 phần
@@ -146,7 +237,7 @@ namespace BatchVideoEditing
 			{
 				if (i % 2 == 0)
 				{
-					string flippedPath = Path.Combine(tempFolderPath, "flipped_" + Path.GetFileName(splitVideos[i]));
+					string flippedPath = Path.Combine(tempFolderPath, "flipped_" + videoIndex + Path.GetFileName(splitVideos[i]));
 					FlipHorizontal(splitVideos[i], flippedPath);
 					splitVideos[i] = flippedPath; // Cập nhật đường dẫn sau khi flip
 				}
@@ -154,19 +245,27 @@ namespace BatchVideoEditing
 			UpdateVideoProgress(progressBar, label, 60); // Cập nhật tiến độ sau khi hoàn thành bước 3
 
 			// 4. Ghép các phần video lại thành 1 video trong temp
-			string combinedPath = Path.Combine(tempFolderPath, "combined_" + Path.GetFileNameWithoutExtension(inputPath) + ".mp4");
-			CombineVideos(splitVideos, combinedPath);
+			string combinedPath = Path.Combine(tempFolderPath, "combined_" + videoIndex + Path.GetFileNameWithoutExtension(inputPath) + ".mp4");
+			CombineVideos(splitVideos, combinedPath, videoIndex);
 			UpdateVideoProgress(progressBar, label, 80); // Cập nhật tiến độ sau khi hoàn thành bước 4
 
 			// 5. Chèn text vào video và lưu vào thư mục final
-			string overlayOutputPath = Path.Combine(finalFolderPath, "overlaytext_" + Path.GetFileNameWithoutExtension(inputPath) + ".mp4");
+			string overlayOutputPath = Path.Combine(finalFolderPath, "overlaytext_" + videoIndex + Path.GetFileNameWithoutExtension(inputPath) + ".mp4");
 			OverlayText(combinedPath, Path.GetFileNameWithoutExtension(inputPath), overlayOutputPath);
 			UpdateVideoProgress(progressBar, label, 95);
 
 			// 6. Tăng sáng video lên 15% và lưu với tên gọi là tên ban đầu + "_final.mp4"
-			string finalOutputPath = Path.Combine(finalFolderPath, Path.GetFileNameWithoutExtension(inputPath) + "_final.mp4");
+			string finalOutputPath = Path.Combine(finalFolderPath, Path.GetFileNameWithoutExtension(inputPath) + videoIndex + "_final.mp4");
 			BrightenVideo(overlayOutputPath, finalOutputPath);
 			UpdateVideoProgress(progressBar, label, 100); // Cập nhật tiến độ sau khi hoàn thành bước 6
+
+			if (type == 0)
+			{
+				++completeVideoAsync;
+				int totalProgressPercentage = (completeVideoAsync * 100) / totalVideoImportAsync;
+				totalprogressBar.Value = totalProgressPercentage;
+				totallblProgress.Text = $"Total Progress: {totalProgressPercentage}%";
+			}
 
 			// 7.Xóa tệp overlayOutputPath
 			if (File.Exists(overlayOutputPath))
@@ -177,7 +276,8 @@ namespace BatchVideoEditing
 			// 8. Xóa mọi thứ trong thư mục temp sau khi hoàn thành
 			foreach (string file in Directory.GetFiles(tempFolderPath))
 			{
-				File.Delete(file);
+				if (File.Exists(file))
+					File.Delete(file);
 			}
 
 			// 9. Nếu bạn cũng muốn xóa các thư mục con (nếu có), bạn có thể sử dụng
@@ -194,15 +294,15 @@ namespace BatchVideoEditing
 		}
 
 		// Cập nhật CropAndBlurVideo để chỉ lưu file tạm trong temp
-		private void CropAndBlurVideo(string inputPath, string outputPath)
+		private void CropAndBlurVideo(string inputPath, string outputPath, int videoIndex)
 		{
 			// Tạo video background với hiệu ứng blur
-			string blurredBackgroundPath = Path.Combine(outputFolderPath, "temp", "blurred_background.mp4");
+			string blurredBackgroundPath = Path.Combine(outputFolderPath, "temp", "blurred_background" + videoIndex + ".mp4");
 			string blurCommand = $"-i \"{inputPath}\" -vf \"scale=iw:ih,boxblur=50:1\" -c:a copy \"{blurredBackgroundPath}\"";
 			ExecuteFFmpegCommand(blurCommand);
 
 			// Cắt video lại còn 80% chiều dài
-			string cropPath = Path.Combine(outputFolderPath, "temp", "cropped_" + Path.GetFileName(inputPath));
+			string cropPath = Path.Combine(outputFolderPath, "temp", "cropped_" + videoIndex + Path.GetFileName(inputPath));
 			string cropCommand = $"-i \"{inputPath}\" -vf \"crop=iw:ih*0.9\" -c:a copy \"{cropPath}\"";
 			ExecuteFFmpegCommand(cropCommand);
 
@@ -344,11 +444,11 @@ namespace BatchVideoEditing
 			}
 		}
 
-		private void CombineVideos(List<string> videos, string outputPath)
+		private void CombineVideos(List<string> videos, string outputPath, int videoIndex)
 		{
 			// Tạo đường dẫn tới file concat_list.txt
 			string tempFolderPath = Path.Combine(outputFolderPath, "temp");
-			string concatFilePath = Path.Combine(tempFolderPath, "concat_list.txt");
+			string concatFilePath = Path.Combine(tempFolderPath, "concat_list" + videoIndex + ".txt");
 
 			// Thay thế "output/temp/" trong đường dẫn video bằng ""
 			var replacedVideos = videos.Select(v => v.Replace(tempFolderPath + Path.DirectorySeparatorChar, "")).ToList();
@@ -365,8 +465,8 @@ namespace BatchVideoEditing
 
 		private void LogErrorToFile(string error)
 		{
-			string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ffmpeg_errors.txt");
-			File.AppendAllText(filePath, $"{DateTime.Now}: {error}{Environment.NewLine}");
+			//string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ffmpeg_errors.txt");
+			//File.AppendAllText(filePath, $"{DateTime.Now}: {error}{Environment.NewLine}");
 		}
 
 		private string ExecuteFFmpegCommand(string arguments, bool returnOutput = false)
@@ -412,12 +512,38 @@ namespace BatchVideoEditing
 
 		private void btnClearAll_Click(object sender, EventArgs e)
 		{
+			totalVideoImportAsync = 0;
+			completeVideoAsync = 0;
 			lstVideos.Items.Clear();
 			flowLayoutPanelProgress.Controls.Clear();
 			videoPaths.Clear();
-			progressBar.Value = 0;
-			lblProgress.Text = "Progress: 0%";
-			btnGoToPath.Visible = false;
+			totalprogressBar.Value = 0;
+			totallblProgress.Text = "Total Progress: 0%";
+			comboBox1.Enabled = true;
+			btnImport.Enabled = true;
+			btnProcess.Enabled = true;
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			comboBox1.SelectedIndex = 0;
+			totalVideoImportAsync = 0;
+			completeVideoAsync = 0;
+		}
+
+		private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			int index = comboBox1.SelectedIndex;
+			switch (index)
+			{
+				case 0:
+					type = 0;
+					break;
+				case 1:
+					type = 1;
+					break;
+				default: break;
+			}
 		}
 	}
 }
